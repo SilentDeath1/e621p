6afbe8cb6632053e842166665755ffff5c2a2dd1
SilentDeath1 <SilentDeath1@users.noreply.github.com>
2024-12-15 00:03:03 +0000

    use url as query params were being incorrectly parsed

--- js/script.js
+++ js/script.js
@@ -158,14 +158,10 @@ $(function () {
     };
 
     function getQueryVariable(variable) {
-     var query = window.location.search.substring(1);
-     var vars = query.split("&");
-     for (var i=0;i<vars.length;i++) {
-           var pair = vars[i].split("=");
-           if(pair[0] == variable){return pair[1];}
+        const url = new URL(window.location)
+        const value = url.searchParams.get(variable)
+        return value || false
      }
-    return(false);
-    }
 
     function open_in_background(selector){
         // as per https://developer.mozilla.org/en-US/docs/Web/API/event.initMouseEvent
